418cbfe4fbabfc4e4cafdcb358a1a6b98cc4d914
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-08-16 20:25:01 +0800

    [To be modified]Add hfml_importer scripts

---
+++ hfml_importer.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from openpecha.formatters import HFMLFormatter
+import os
+
+
+ROOTDIR= os.getcwd()
+
+# hfml_fn = Path("tests") / "formatters" / "hfml" / "data" / "kangyur_01.txt"
+hfml_fn = Path(ROOTDIR+"/output/T12/vol1")
+formatter = HFMLFormatter(output_path="opf/t12/vol1")
+formatter.create_opf(hfml_fn)
