9efa9a5e287c6c05d84e17148cf6ffbdbabbb76a
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-09-03 14:25:55 +0800

    Add Excel files for testing

---
+++ extract_dictionaries.py
@@ -0,0 +1,22 @@
+
+import pandas as pd 
+
+# Billingual Corpus (for Azure)
+## Dictionary-1
+## Read Excel Files
+files = str(input("Enter the Excel files path:"))
+df = pd.read_excel(files, sheet_name=None)
+print(df)
+## Dictionary-4
+## Dictionary-5
+## Dictionary-6
+
+# Billingual Dictionary
+
+## Dictionary-2
+## Dictionary-3
+
+## Dictionary-1
+## Dictionary-4
+## Dictionary-5
+## Dictionary-6
